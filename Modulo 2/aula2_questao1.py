0038f59181048797e4480df9d5f86afa1b75e518
César Augusto <153289960+CesarAugusto21@users.noreply.github.com>
2024-05-22 16:04:07 -0300

    Add files via upload

---
+++ Modulo 2/aula2_questao1.py
@@ -0,0 +1,19 @@
+num1 = 5 
+num2 = 2
+print(type(num1))
+print(type(num2))
+divisao = num1 / num2 
+print(type(divisao))
+print(divisao)
+texto = "o resultado é"
+num3 = 10
+num4 = 3.5
+soma = num3 + num4
+print (texto)
+print (num3)
+print(num4)
+print(soma)
+print(type(texto))
+print(type(num3))
+print(type(num4))
+print(type(soma))
---
+++ Modulo 2/aula2_questao3.py
@@ -0,0 +1,5 @@
+v1 = 10 
+v2 = 20
+troca = 2
+v1 = v1 * troca
+v2 = v2 / troca
