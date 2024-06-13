203e3dd285cfc1912d4bdaa292a897d1a77a2e6e
César Augusto <153289960+CesarAugusto21@users.noreply.github.com>
2024-06-13 10:48:26 -0300

    Update aula4_questao2.py

--- Modulo 2/aula4_questao2.py
+++ Modulo 2/aula4_questao2.py
@@ -4,7 +4,7 @@
 #Antes de imprimir, converta o valor em Celsius para inteiro. A mensagem deve estar formatada da seguinte maneira:
 #86 graus Fahrenheit são 30 graus Celsius.
 
-f= int(input("qual a temperatura em Fahrenheit?"))
+f= int(input("Qual a temperatura em Fahrenheit: "))
 c = (f - 32) * (5 / 9)
 c = int (c)
 print(f"{f} graus Fahrenheit são {c} graus Celsius")
