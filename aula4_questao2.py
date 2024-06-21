5377a1a47362dc2ecb40ff037c3ed11996af0b66
César Augusto <153289960+CesarAugusto21@users.noreply.github.com>
2024-06-20 21:48:13 -0300

    Delete aula4_questao2.py

--- aula4_questao2.py
+++
@@ -1,9 +0,0 @@
-frase = input("Digite uma frase: ")
-vogais = "aeiouAEIOU"
-
-vogaisFrase = [char for char in frase if char in vogais]
-consoantesFrase = [char for char in frase if char not in vogais]
-vogaisOrdenadas = sorted(vogaisFrase)
-
-print("Vogais:", vogaisOrdenadas)
-print("Consoantes:", consoantesFrase)
